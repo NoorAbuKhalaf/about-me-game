f02a210288dd33e2f830f5a03ec6178c114e3639
NoorAbuKhalaf <noor.abukhalaf@hotmail.com>
2021-03-15 16:23:44 +0200

    Edited by Noor

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # about-me-game
+**-Questions Game-** 
+## it is a website to know information about me by expecting answers 
---
+++ js/app.js
@@ -0,0 +1,61 @@
+'use strict';
+
+alert ('Hello Welcome here to know about me');
+let age = prompt('Am I 25 years old?');
+if (age.toLowerCase() === 'yes')
+{
+  //console.log('no it is 29!');
+  alert ('no it is 29!');
+}
+else {
+  //console.log('you are right, I am 29!');
+  alert ('you are right, I am 29!');
+}
+
+let prof = prompt('Am I a software developer?');
+if (prof.toLowerCase() === 'yes')
+{
+  // console.log('no, an architect!');
+  alert ('no, an architect!');
+}
+else {
+  //console.log('you are right, I am an architect!');
+  alert ('you are right, I am an architect!');
+}
+
+let intrest = prompt('Do you think I like photography?');
+if (intrest.toLowerCase() === 'yes')
+{
+  // console.log('you are right');
+  alert ('you are right');
+}
+else {
+  //console.log(':( you're wrong I like it');
+  alert ('you are wrong I like it');
+}
+
+let like = prompt('Do you think I like cats?');
+if (like.toLowerCase() === 'yes')
+{
+  // console.log('you are right');
+  alert ('you are right');
+}
+else {
+  //console.log(':( you're wrong I like it');
+  alert ('you are wrong I like it');
+}
+
+let color = prompt('Do you think I like Blue?');
+if (color.toLowerCase() === 'yes')
+{
+  // console.log('you are right and I like red also');
+  alert ('you are right and I like red also');
+}
+else {
+  //console.log(':( you're wrong I like it');
+  alert ('you are wrong I like it');
+}
+
+let yourName = prompt('what is your name?');
+alert ('Welcome here and thank you, '+yourName);
+
