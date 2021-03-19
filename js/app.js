1b48288861b950c8b8aee65f1cdcb03f0b91cb61
NoorAbuKhalaf <noor.abukhalaf@hotmail.com>
2021-03-19 23:35:56 +0200

    final

--- js/app.js
+++ js/app.js
@@ -1,129 +1,129 @@
 'use strict';
-// let Score =0;
-// alert ('Hello Welcome here to know about me');
+let Score =0;
+alert ('Hello Welcome here to know about me');
 
-// function age() {
-
-
-//   let age = prompt('Am I 25 years old?');
-//   if (age.toLowerCase() === 'yes')
-//   {
-//     alert ('no it is 29!');
-//   }
-//   else {
-//     alert ('you are right, I am 29!');
-//     Score ++;
-
-//   }}
-// age();
+function age() {
 
 
-// function prof() {
+  let age = prompt('Am I 25 years old?');
+  if (age.toLowerCase() === 'yes')
+  {
+    alert ('no it is 29!');
+  }
+  else {
+    alert ('you are right, I am 29!');
+    Score ++;
 
-//   let prof = prompt('Am I a software developer?');
-//   if (prof.toLowerCase() === 'yes')
-//   {
+  }}
+age();
 
-//     alert ('no, an architect!');
-//   }
-//   else {
 
-//     alert ('you are right, I am an architect!');
-//     Score ++;
-//   }}
-// prof();
-// function photo() {
-//   let intrest = prompt('Do you think I like photography?');
-//   if (intrest.toLowerCase() === 'yes')
-//   {
+function prof() {
 
-//     alert ('you are right');
-//     Score ++;
-//   }
-//   else {
+  let prof = prompt('Am I a software developer?');
+  if (prof.toLowerCase() === 'yes')
+  {
 
-//     alert ('you are wrong I like it');
-//   }
-// }
-// photo();
+    alert ('no, an architect!');
+  }
+  else {
 
-// function cat(){
-//   let like = prompt('Do you think I like cats?');
-//   if (like.toLowerCase() === 'yes')
-//   {
+    alert ('you are right, I am an architect!');
+    Score ++;
+  }}
+prof();
+function photo() {
+  let intrest = prompt('Do you think I like photography?');
+  if (intrest.toLowerCase() === 'yes')
+  {
 
-//     alert ('you are right');
-//     Score ++;
-//   }
-//   else {
+    alert ('you are right');
+    Score ++;
+  }
+  else {
 
-//     alert ('you are wrong I like it');
-//   }
-// }
-// cat();
+    alert ('you are wrong I like it');
+  }
+}
+photo();
 
-// function blue(){
-//   let color = prompt('Do you think I like Blue?');
-//   if (color.toLowerCase() === 'yes')
-//   {
+function cat(){
+  let like = prompt('Do you think I like cats?');
+  if (like.toLowerCase() === 'yes')
+  {
 
-//     alert ('you are right and I like red also');
-//     Score ++;
-//   }
-//   else {
+    alert ('you are right');
+    Score ++;
+  }
+  else {
 
-//     alert ('you are wrong I like it');
-//   }
-// }
-// blue();
+    alert ('you are wrong I like it');
+  }
+}
+cat();
 
-// let yourName = prompt('what is your name?');
-// alert ('Welcome nice to meet you, '+yourName);
+function blue(){
+  let color = prompt('Do you think I like Blue?');
+  if (color.toLowerCase() === 'yes')
+  {
 
-// //question 6
-// function numb(){
-//   for( let i=0; i<=3; i=i+1){
-//     let num=prompt('guess the correct number I liked form 1-15');
-//     if (num<5) {
-//       alert ('No, too low');
-//     } else if (num>5) {
-//       alert ('No, too high');
-//     }
-//     else{
-//       alert ('yeaaah! it is 5');
-//       Score ++;
-//       break;
-//     }
-//   }
+    alert ('you are right and I like red also');
+    Score ++;
+  }
+  else {
 
-//   alert ('the right answer is 5');
-// }
-// numb();
-// //question 7
+    alert ('you are wrong I like it');
+  }
+}
+blue();
 
-// function movie(){
-//   let myMovies=['lionking','mulan','cars'];
-//   for( let i=0; i<=5; i=i+1){
-//     let movie=prompt('can you guess what is my fav. disney movie');
-//     if (movie===myMovies[0]) {
-//       alert ('yeaaah you alright');
-//       Score ++;
-//       break;
-//     } else if (movie===myMovies[1]) {
-//       alert ('yeaaah you alright');
-//       Score ++;
-//       break;
-//     }
-//     else if (movie===myMovies[2]){
-//       alert ('yeaaah you alright');
-//       Score ++;
-//       break;
-//     }
-//     else{
-//       alert ('try again');
-//     }
-//   }
-//   alert('my fav. movies is ' + myMovies);
-// }
-// movie();
-// alert('your Score is:  ' + Score +' from 7 Ques');
+let yourName = prompt('what is your name?');
+alert ('Welcome nice to meet you, '+yourName);
+
+//question 6
+function numb(){
+  for( let i=0; i<=3; i=i+1){
+    let num=prompt('guess the correct number I liked form 1-15');
+    if (num<5) {
+      alert ('No, too low');
+    } else if (num>5) {
+      alert ('No, too high');
+    }
+    else{
+      alert ('yeaaah! it is 5');
+      Score ++;
+      break;
+    }
+  }
+
+  alert ('the right answer is 5');
+}
+numb();
+//question 7
+
+function movie(){
+  let myMovies=['lionking','mulan','cars'];
+  for( let i=0; i<=5; i=i+1){
+    let movie=prompt('can you guess what is my fav. disney movie');
+    if (movie===myMovies[0]) {
+      alert ('yeaaah you alright');
+      Score ++;
+      break;
+    } else if (movie===myMovies[1]) {
+      alert ('yeaaah you alright');
+      Score ++;
+      break;
+    }
+    else if (movie===myMovies[2]){
+      alert ('yeaaah you alright');
+      Score ++;
+      break;
+    }
+    else{
+      alert ('try again');
+    }
+  }
+  alert('my fav. movies is ' + myMovies);
+}
+movie();
+alert('your Score is:  ' + Score +' from 7 Ques');
